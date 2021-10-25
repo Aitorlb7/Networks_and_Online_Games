c8630ff8c03e18166ed1ef1b2a08bff200ba051c
Ángel González <angotov@gmail.com>
2021-10-25 10:33:30 +0200

    Started to modify the TCP_Client to fit the specified specs.

---
+++ Chat Application/Assets/Scripts/Client_Generator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client_Generator : MonoBehaviour
+{
+    public GameObject clientExample;
+    public Text clientName;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GenerateClient()
+    {
+        GameObject newClient = GameObject.Instantiate(clientExample);
+        newClient.SetActive(true);
+    }
+}
--- Chat Application/Assets/Scripts/TCP_Client.cs
+++ Chat Application/Assets/Scripts/TCP_Client.cs
@@ -7,34 +7,28 @@ using System.Text;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class TCP_Client : MonoBehaviour
 {
+    private Socket          socket = null;
+    private EndPoint        remoteIP;
 
-    private Thread thread = null;
+    private string          IP = "127.0.0.1";
 
-    private Socket socket = null;
-    private EndPoint remoteIP;
+    private byte[]          data = new byte[1024];
 
-    private string IP = "127.0.0.1";
+    private int             recievedData;
+    private string          recievedMessage;
 
-    private byte[] data = new byte[1024];
+    private bool            breakAndDisconect;
 
-    private int recievedData;
-    private string recievedMessage;
+    public int              destPort;
+    public Text             inputText;
+    public Text             chatText;
 
-    private readonly object lockConsole = new object();
-    private bool breakAndDisconect;
-
-
-    public int destPort;
-    public string message;
-    public int sleepSeconds;
-    public Text consoleText;
-
-    private List<string> consoleStrings = new List<string>();
-    private bool updateConsole;
-    private bool clearConsole;
-    private int pongNum = 0;
+    private List<string>    chatMessages = new List<string>();
+    private bool            updateChat;
+    private bool            clearChat;
 
     void Start()
     {
@@ -43,38 +37,32 @@ public class TCP_Client : MonoBehaviour
         remoteIP = new IPEndPoint(IPAddress.Parse(IP), destPort);
 
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        thread = new Thread(Listen);
-
-        thread.Start();
-        //ConnectToServer();
-
     }
+
     void Update()
     {
-        if (updateConsole)
+        if (updateChat)
         {
-            lock (lockConsole)
+            if (clearChat)
             {
-                if (clearConsole)
-                {
-                    consoleText.text = "";
-                    clearConsole = false;
-                }
-
-                foreach (string message in consoleStrings)
-                    consoleText.text += message + '\n';
-
-                consoleStrings.Clear();
-
-                updateConsole = false;
+                chatText.text = "";
+                clearChat = false;
             }
+
+            foreach (string message in chatMessages)
+                chatText.text += message + '\n';
+
+            chatMessages.Clear();
+
+            updateChat = false;
         }
     }
     private void Listen()
     {
-        if(socket.Connected) 
+        if(socket.Connected)
+        {
             SendMessage();
+        }
         else
         {
             ConnectToServer();
@@ -85,9 +73,10 @@ public class TCP_Client : MonoBehaviour
         {
             recievedData = socket.ReceiveFrom(data, ref remoteIP);
 
-            if ( breakAndDisconect || pongNum >= 5)
+            if (breakAndDisconect)
+            {
                 break;
-
+            }
 
             recievedMessage = Encoding.ASCII.GetString(data, 0, recievedData);
 
@@ -95,13 +84,7 @@ public class TCP_Client : MonoBehaviour
 
             AddTextToConsole("Recieved: " + recievedMessage);
 
-            pongNum++;
-
-            Thread.Sleep(sleepSeconds);
-
             SendMessage();
-
-
         }
 
         AddTextToConsole("Client disconnected from server");
@@ -120,14 +103,13 @@ public class TCP_Client : MonoBehaviour
     {
         try
         {
-            data = Encoding.ASCII.GetBytes(message);
+            data = Encoding.ASCII.GetBytes(/*message*/ inputText.text);
 
             socket.SendTo(data, remoteIP);
 
             data = new byte[1024];
 
-            AddTextToConsole("Sent: " + message);
-
+            //AddTextToConsole("Sent: " + message);
         }
         catch (Exception e)
         {
@@ -148,31 +130,28 @@ public class TCP_Client : MonoBehaviour
         {
             AddTextToConsole("Unable to connect to server.");
             AddTextToConsole(e.ToString());
-            return;
         }
     }
 
-    public void CreateNewClient()
-    {
-        GameObject.Instantiate(GameObject.Find("TCP_Client"));
-    }
+    //public void CreateNewClient()
+    //{
+    //    GameObject.Instantiate(GameObject.Find("TCP_Client"));
+    //}
 
     private void AddTextToConsole(string textToAdd)
     {
-        lock (lockConsole)
-        {
-            consoleStrings.Add(textToAdd);
+        chatMessages.Add(textToAdd);
 
-            updateConsole = true;
-        }
+        updateChat = true;
     }
 
     private void ClearClientConsole()
     {
-        consoleStrings.Clear();
-        clearConsole = true;
-        updateConsole = true;
+        chatMessages.Clear();
+        clearChat = true;
+        updateChat = true;
     }
+
     public void DisconnectAndDestroy()
     {
         breakAndDisconect = true;
@@ -180,19 +159,12 @@ public class TCP_Client : MonoBehaviour
 
     private void OnDestroy()
     {
-        Debug.Log("Disconecting Client and aborting Thread");
+        Debug.Log("Disconecting Client Socket");
 
         if (socket != null)
         {
             socket.Close();
             socket = null;
         }
-
-        if (thread.IsAlive)
-        {
-            thread.Abort();
-            thread = null;
-        }
-        
     }
 }
